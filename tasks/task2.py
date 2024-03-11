b803db0385542c3ae6c7f488deb6414f94f9cbb9
Zamiq88 <161586578+Zamiq88@users.noreply.github.com>
2024-03-11 10:26:16 +0400

    Add files via upload

---
+++ tasks/task2.py
@@ -0,0 +1,119 @@
+#task1
+#sum=0
+#list1=[2,4,7,8,3]
+#for i in list1:
+ #   sum=sum+i 
+#print(sum)
+
+#mul=1
+#list2=(2,5,3,8,5)
+#for x in list2:
+ #   mul*=x
+#print(mul)    
+
+
+
+
+#task2
+#lista=['assd', 5 ,7, 'kjd',8]
+#listb=lista[::-1]
+#print(listb)
+
+#task 3
+#list1=['sa','me','ad']
+#list2=['lam','nim', 'im']
+#result=[]
+#a=zip(list1,list2)
+#for x,y in a:
+ #   x=x+y
+  #  result.append(x)
+#print(result) 
+
+#task4
+#a=[[7,9,3,2],['df','uyt','kj'],[9,5,3,2,1]]
+#a[2].insert(2,'kjh')
+#print(a)
+
+#task 5
+#list3=[8,5,2,4,9]
+#print(list3.index(9))
+#list3[4]=7
+#print(list3)
+
+#task6
+# list7=[7,8,2,6,4]
+# sum=0
+
+# for x in  list7:
+#     sum+=x
+# a=sum/len(list7)    
+# print(a)
+
+#task7
+#a=int(input('1-ci reqem'))
+#b=int(input('2-ci reqem'))
+#c=[] #cut ededler
+#d=[] #tek ededler
+#for x in range(a,b):
+ #   if x%2==0:
+  #      c.append(x)
+   # else:
+    #    d.append(x)    
+#print('cut ededler:',c)
+#print('tek ededler:' , d)
+#print('cut reqemlerin sayi:' , len(c))
+
+ 
+
+#task8
+#list1=[5,6,7,5,7,4,7]  
+#list2=set(list1)
+#list3=list(list2)
+#print(list3)
+
+#task 9
+
+#list1=[1,2,3]
+
+
+# for a in range(1,4):
+#     list1[0]=a
+    
+    
+        
+#     for a in range (1,4):
+#         list1[1]=a
+#         for a in range(1,4):
+#             list1[2]=a
+    
+#             if len(set(list1))<3:
+#                 continue
+#             print(list1)  
+
+#task 9a
+# a = {'ad':['Fuad','Rovsen','Orxan']}
+# secim=input('Secim Edin : (1) Elave et (2) Sil')
+# if secim=='1':
+#     ad=input('ad daxil edin')
+#     a['ad'].append(ad)
+# if secim=='2':
+#     ad2=input('silmek istediyiniz adi yazin')
+#     if ad2 in a['ad']:
+#         a['ad'].remove(ad2)
+#     elif ad2=='':
+#         print('ad daxil etmediniz')     
+#     else:
+#         print(' Bu ad bazada yoxdur')
+     
+# print(a)              
+
+#task10
+
+# a=input('daxil edin')
+# x=0
+# while x<6:
+  
+#     x+=1
+#     print(x, a)
+
+
